f7b1ba37e22691144beb9c7fd725559f7f575524
William Vedroni <william.silva@s2it.com.br>
2015-04-02 10:51:34 -0300

    fix - ajustment validation

--- networkapi/queue_tools/queue_manager.py
+++ networkapi/queue_tools/queue_manager.py
@@ -59,8 +59,8 @@ class QueueManager(object):
         """
 
         try:
-            if types.DictType != type(dict_obj):
-                raise ValueError(u"QueueManagerError - The type must be a Dict")
+            if not isinstance(dict_obj, types.DictType):
+                raise ValueError(u"QueueManagerError - The type must be a instance of Dict")
 
             self._queue.append(dict_obj)
 
