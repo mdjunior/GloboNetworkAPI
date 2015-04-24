6a632b3efdc98eac435b5629407b9d603a6ecce6
William Vedroni <william.silva@s2it.com.br>
2015-04-24 12:05:57 -0300

    fix - change import settiings

--- networkapi/settings_test.py
+++ networkapi/settings_test.py
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from django.conf.settings import *
+from networkapi.settings import *
 
 
 DATABASES = {
