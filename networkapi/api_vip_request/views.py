e6fc473875f872ae2b0ec67cd6c7fb0b8aabc29a
William Vedroni da Silva | S2IT <W3S\wsilva@ub-aqa-178.w3s.local>
2015-05-05 10:21:21 -0300

    refactor removing old compatibility

--- networkapi/api_vip_request/views.py
+++ networkapi/api_vip_request/views.py
@@ -26,7 +26,7 @@ from networkapi.log import Log
 from networkapi.distributedlock import distributedlock, LOCK_VIP
 from networkapi.api_vip_request.permissions import Read, Write
 from networkapi.requisicaovips.models import ServerPool, VipPortToPool, \
-    RequisicaoVips
+    RequisicaoVips, RequisicaoVipsError
 from networkapi.api_rest import exceptions as api_exceptions
 from networkapi.api_pools import exceptions as pool_exceptions
 from networkapi.api_vip_request import exceptions
@@ -157,9 +157,6 @@ def list_environment_by_environment_vip(request, environment_vip_id):
         raise api_exceptions.NetworkAPIException()
 
 
-
-
-
 @api_view(['POST', 'PUT'])
 @permission_classes((IsAuthenticated, Write))
 @commit_on_success
@@ -167,118 +164,33 @@ def save(request, pk=None):
 
     try:
 
-        data = request.DATA
-        user = request.user
-
-        vip_ports = data.get("vip_ports_to_pools")
-
-        req_vip_serializer = RequestVipSerializer(
-            data=data
-        )
-
-        if not req_vip_serializer.is_valid():
-            raise api_exceptions.ValidationException("Invalid Vip Request")
-
         if request.method == "POST":
 
-            obj_req_vip = req_vip_serializer.object
+            data = facade.save(request)
 
-            obj_req_vip.filter_valid = True
-            obj_req_vip.validado = False
-            facade.set_l7_filter_for_vip(obj_req_vip)
-            obj_req_vip.set_new_variables(data)
-            obj_req_vip.save(user)
-
-            if vip_ports:
-
-                for v_port in obj_req_vip.vip_ports_to_pools:
-                    v_port.requisicao_vip = obj_req_vip
-                    v_port.save(user)
-            else:
-                facade.validate_reals(data)
-                obj_req_vip.save_vips_and_ports(data, user)
-
-            return Response(
-                data=req_vip_serializer.data,
-                status=status.HTTP_201_CREATED
-            )
+            return Response(data=data, status=status.HTTP_201_CREATED)
 
         elif request.method == "PUT":
 
-            RequisicaoVips.objects.get(pk=pk)
+            data = facade.update(request, pk)
 
-            with distributedlock(LOCK_VIP % pk):
+            return Response(data=data)
 
-                obj_req_vip = req_vip_serializer.object
-                obj_req_vip.id = pk
-                obj_req_vip.filter_valid = True
-                obj_req_vip.validado = False
-                facade.set_l7_filter_for_vip(obj_req_vip)
-                obj_req_vip.set_new_variables(data)
-                obj_req_vip.save(user)
+    except EnvironmentVipNotFoundError, error:
+        log.error(error)
+        raise exceptions.EnvironmentVipDoesNotExistException()
 
-                if vip_ports:
-
-                    vip_port_serializer = VipPortToPoolSerializer(data=vip_ports, many=True)
-
-                    if not vip_port_serializer.is_valid():
-                        raise api_exceptions.ValidationException("Invalid Port Vip To Pool")
-
-                    vip_port_to_pool_pks = [port['id'] for port in vip_ports if port.get('id')]
-
-                    vip_port_to_pool_to_remove = VipPortToPool.objects.filter(
-                        requisicao_vip=obj_req_vip
-                    ).exclude(
-                        id__in=vip_port_to_pool_pks
-                    )
-
-                    for v_port_to_del in vip_port_to_pool_to_remove:
-                        v_port_to_del.delete(user)
-
-                    for vip_port in vip_ports:
-                        vip_port_obj = VipPortToPool()
-                        vip_port_obj.id = vip_port.get('id')
-                        vip_port_obj.server_pool = ServerPool(id=vip_port.get('server_pool'))
-                        vip_port_obj.port_vip = vip_port.get('port_vip')
-                        vip_port_obj.requisicao_vip = obj_req_vip
-                        vip_port_obj.save(user)
-
-                else:
-                    facade.validate_reals(data)
-                    obj_req_vip.delete_vips_and_reals(user)
-                    obj_req_vip.save_vips_and_ports(data, user)
-
-                return Response(data=req_vip_serializer.data)
-
-    except EnvironmentVipNotFoundError, exception:
-        log.error(exception.message)
-        raise api_exceptions.ValidationException(exception.message)
-
-    except EquipamentoError, exception:
-        log.error(exception.message)
-        raise api_exceptions.ValidationException(exception.message)
-
-    except EquipamentoNotFoundError, exception:
-        log.error(exception.message)
-        raise api_exceptions.ValidationException(exception.message)
-
-    except IpNotFoundByEquipAndVipError, exception:
-        log.error(exception.message)
-        raise api_exceptions.ValidationException(exception.message)
-
-    except InvalidValueError, exception:
-        message = u'Invalid Ip Type'
+    except exceptions.InvalidIdVipRequestException, exception:
         log.error(exception)
-        log.error(message)
-        raise api_exceptions.ValidationException(message)
+        raise exception
 
     except RequisicaoVips.DoesNotExist, exception:
         log.error(exception)
         raise exceptions.VipRequestDoesNotExistException()
 
-    except api_exceptions.ValidationException, exception:
+    except (InvalidValueError, RequisicaoVipsError, api_exceptions.ValidationException), exception:
         log.error(exception)
-        raise api_exceptions.ValidationException(exception.message)
+        raise api_exceptions.ValidationException()
 
     except Exception, exception:
         log.error(exception)
