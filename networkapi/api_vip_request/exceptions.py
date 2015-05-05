b95ab9c77cb056699897bf9a44f5f0f9f235ef23
William Vedroni da Silva | S2IT <W3S\wsilva@ub-aqa-178.w3s.local>
2015-05-05 10:17:06 -0300

    add exception to environment vip

--- networkapi/api_vip_request/exceptions.py
+++ networkapi/api_vip_request/exceptions.py
@@ -7,6 +7,10 @@ class VipRequestDoesNotExistException(APIException):
     default_detail = 'Vip Request Does Not Exist.'
 
 
+class EnvironmentVipDoesNotExistException(APIException):
+    status_code = status.HTTP_404_NOT_FOUND
+    default_detail = 'Environment Vip Does Not Exist.'
+
 class InvalidIdVipRequestException(APIException):
     status_code = status.HTTP_400_BAD_REQUEST
     default_detail = 'Invalid id for Vip Request.'
