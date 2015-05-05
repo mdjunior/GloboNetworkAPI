60b4547894898e4e7ffaa3d7cfaba2aaa0790c37
William Vedroni da Silva | S2IT <W3S\wsilva@ub-aqa-178.w3s.local>
2015-05-05 10:19:55 -0300

    refactor save/update vip request for facade

--- networkapi/api_vip_request/facade.py
+++ networkapi/api_vip_request/facade.py
@@ -17,14 +17,17 @@
  limitations under the License.
  """
 from django.forms import model_to_dict
+from networkapi.distributedlock import distributedlock, LOCK_VIP
+from networkapi.log import Log
 from networkapi.ambiente.models import EnvironmentVip
+from networkapi.api_vip_request.serializers import RequestVipSerializer, VipPortToPoolSerializer
 from networkapi.equipamento.models import Equipamento
-
-from networkapi.requisicaovips.models import RequisicaoVips, VipPortToPool
-from networkapi.util import is_valid_int_greater_zero_param, convert_boolean_to_int, mount_ipv4_string, \
-    mount_ipv6_string
+from networkapi.requisicaovips.models import RequisicaoVips, VipPortToPool, ServerPool
+from networkapi.util import is_valid_int_greater_zero_param, convert_boolean_to_int
 from networkapi.api_vip_request import exceptions
 from networkapi.api_rest import exceptions as api_exceptions
+
+log = Log(__name__)
 
 
 def get_by_pk(pk):
@@ -43,6 +46,14 @@ def get_by_pk(pk):
     vip_request = RequisicaoVips.objects.get(id=pk)
 
     data = vip_request.variables_to_map()
+    data['id'] = vip_request.id
+    data['validado'] = convert_boolean_to_int(vip_request.validado)
+    data['vip_criado'] = convert_boolean_to_int(vip_request.vip_criado)
+    data['id_ip'] = vip_request.ip_id
+    data['id_ipv6'] = vip_request.ipv6_id
+    data['id_healthcheck_expect'] = vip_request.healthcheck_expect_id
+    data['l7_filter'] = vip_request.l7_filter
+    data['rule_id'] = vip_request.rule_id
 
     pools = []
 
@@ -58,89 +69,127 @@ def get_by_pk(pk):
         pool_raw["port_vip_id"] = vip_port.id
 
         for pool_member in server_pool.serverpoolmember_set.all():
-
             pools_member_raw = model_to_dict(pool_member)
-
             ipv4 = pool_member.ip
             ipv6 = pool_member.ipv6
             ip_equipment_set = ipv4 and ipv4.ipequipamento_set or ipv6 and ipv6.ipv6equipament_set
             ip_equipment_obj = ip_equipment_set.select_related().uniqueResult()
-
-            healthcheck_type = pool_member.healthcheck and pool_member.healthcheck.healthcheck_type
-
+            healthcheck_type = pool_member.healthcheck and pool_member.healthcheck.healthcheck_type or ''
             pools_member_raw['healthcheck'] = {'healthcheck_type': healthcheck_type}
             pools_member_raw['equipment_name'] = ip_equipment_obj.equipamento.nome
-
-            ip_formated = ip_equipment_obj.ip.ip_formated
+            ip_formatted = ip_equipment_obj.ip.ip_formated
 
             if ipv4:
-                pools_member_raw["ip"] = {'ip_formated': ip_formated}
+                pools_member_raw["ip"] = {'ip_formated': ip_formatted}
             else:
-                pools_member_raw["ipv6"] = {'ip_formated': ip_formated}
+                pools_member_raw["ipv6"] = {'ip_formated': ip_formatted}
 
             pools_members.append(pools_member_raw)
 
         pool_raw['server_pool_members'] = pools_members
-
         pools.append(pool_raw)
 
     data["pools"] = pools
 
-    vip_port_list, reals_list, reals_priority, reals_weight = get_vips_and_reals(
-        vip_request.id,
-    )
-
-    if reals_list:
-        data['reals'] = {'real': reals_list}
-        data['reals_prioritys'] = {'reals_priority': reals_priority}
-        data['reals_weights'] = {'reals_weight': reals_weight}
-
-    data['portas_servicos'] = vip_port_list
-    data['id'] = vip_request.id
-    data['validado'] = convert_boolean_to_int(vip_request.validado)
-    data['vip_criado'] = convert_boolean_to_int(vip_request.vip_criado)
-    data['id_ip'] = vip_request.ip_id
-    data['id_ipv6'] = vip_request.ipv6_id
-    data['id_healthcheck_expect'] = vip_request.healthcheck_expect_id
-    data['l7_filter'] = vip_request.l7_filter
-    data['rule_id'] = vip_request.rule_id
-
     return data
 
 
-def validate_reals(data):
+def save(request):
     """
-    Validate Reals to save/update
-    :param data: Reals
-    :return:
+    Save Vip Request
+
+    :param request: Request
+    :return: Data Serialized Post Save
     """
 
-    reals_data = data.get('reals')
+    data = request.DATA
+    user = request.user
 
-    if reals_data is not None:
+    req_vip_serializer = RequestVipSerializer(
+        data=data
+    )
 
-        finality = data.get('finalidade')
-        client = data.get('cliente')
-        environment = data.get('ambiente')
+    if not req_vip_serializer.is_valid():
+        log.error(req_vip_serializer.errors)
+        raise api_exceptions.ValidationException()
 
-        environment_vip = EnvironmentVip.get_by_values(
-            finality,
-            client,
-            environment
+    obj_req_vip = req_vip_serializer.object
+
+    obj_req_vip.filter_valid = True
+    obj_req_vip.validado = False
+    set_l7_filter_for_vip(obj_req_vip)
+    obj_req_vip.set_new_variables(data)
+    obj_req_vip.save(user)
+
+    for v_port in obj_req_vip.vip_ports_to_pools:
+        v_port.requisicao_vip = obj_req_vip
+        v_port.save(user)
+
+    return req_vip_serializer.data
+
+
+def update(request, pk):
+    """
+    Update Vip Request
+
+    :param request:
+    :param pk: Identifier Vip Request
+    :return: Data Serialized Post Update
+    """
+
+    data = request.DATA
+    user = request.user
+
+    if not is_valid_int_greater_zero_param(pk):
+        raise exceptions.InvalidIdVipRequestException()
+
+    vip_ports = data.get("vip_ports_to_pools")
+
+    req_vip_serializer = RequestVipSerializer(
+        data=data
+    )
+
+    if not req_vip_serializer.is_valid():
+        log.error(req_vip_serializer.errors)
+        raise api_exceptions.ValidationException()
+
+    RequisicaoVips.objects.get(pk=pk)
+
+    with distributedlock(LOCK_VIP % pk):
+
+        obj_req_vip = req_vip_serializer.object
+        obj_req_vip.id = pk
+        obj_req_vip.filter_valid = True
+        obj_req_vip.validado = False
+        set_l7_filter_for_vip(obj_req_vip)
+        obj_req_vip.set_new_variables(data)
+        obj_req_vip.save(user)
+
+        vip_port_serializer = VipPortToPoolSerializer(data=vip_ports, many=True)
+
+        if not vip_port_serializer.is_valid():
+            raise api_exceptions.ValidationException("Invalid Port Vip To Pool")
+
+        vip_port_to_pool_pks = [port['id'] for port in vip_ports if port.get('id')]
+
+        vip_port_to_pool_to_remove = VipPortToPool.objects.filter(
+            requisicao_vip=obj_req_vip
+        ).exclude(
+            id__in=vip_port_to_pool_pks
         )
 
-        for real in reals_data.get('real'):
+        for v_port_to_del in vip_port_to_pool_to_remove:
+            v_port_to_del.delete(user)
 
-            real_ip = real.get('real_ip')
-            real_name = real.get('real_name')
+        for vip_port in vip_ports:
+            vip_port_obj = VipPortToPool()
+            vip_port_obj.id = vip_port.get('id')
+            vip_port_obj.server_pool = ServerPool(id=vip_port.get('server_pool'))
+            vip_port_obj.port_vip = vip_port.get('port_vip')
+            vip_port_obj.requisicao_vip = obj_req_vip
+            vip_port_obj.save(user)
 
-            if real_name:
-                equip = Equipamento.get_by_name(real_name)
-            else:
-                message = u'The real_name parameter is not a valid value'
-                raise api_exceptions.ValidationException(message)
-
-            RequisicaoVips.valid_real_server(real_ip, equip, environment_vip, False)
+        return req_vip_serializer.data
 
 
 def set_l7_filter_for_vip(obj_req_vip):
@@ -152,52 +201,3 @@ def set_l7_filter_for_vip(obj_req_vip):
                 flat=True
             )
         )
-
-
-def get_vips_and_reals(id_vip, omit_port_real=False):
-
-    vip_ports = VipPortToPool.get_by_vip_id(id_vip)
-
-    vip_port_list = list()
-    reals_list = list()
-    reals_priority = list()
-    reals_weight = list()
-
-    for v_port in vip_ports:
-        full_port = str(v_port.port_vip)
-
-        if not omit_port_real:
-            full_port += ':' + str(v_port.server_pool.default_port)
-
-        if full_port not in vip_port_list:
-            vip_port_list.append({'porta': full_port, 'vip_port_id': v_port.id })
-
-        members = v_port.server_pool.serverpoolmember_set.all()
-
-        for member in members:
-            try:
-                ip_equip = member.ip.ipequipamento_set.all().uniqueResult()
-                equip_name = ip_equip.equipamento.nome
-                ip_string = mount_ipv4_string(member.ip)
-                ip_id = member.ip.id
-            except:
-                ip_equip = member.ipv6.ipv6equipament_set.all().uniqueResult()
-                equip_name = ip_equip.equipamento.nome
-                ip_string = mount_ipv6_string(member.ipv6)
-                ip_id = member.ipv6.id
-
-            real_raw = {
-                'real_ip': ip_string,
-                'real_name': equip_name,
-                'port_vip': v_port.port_vip,
-                'port_real': member.port_real,
-                'id_ip': ip_id
-            }
-
-            if real_raw not in reals_list:
-                reals_list.append(real_raw)
-
-            reals_priority.append(member.priority)
-            reals_weight.append(member.weight)
-
-    return vip_port_list, reals_list, reals_priority, reals_weight
